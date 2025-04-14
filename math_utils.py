2238540e39733fa7b81057e1f7757fb5fe209e0f
Wagner Santos <163296736+wagnsantos@users.noreply.github.com>
2025-04-14 20:36:12 -0300

    Create math_utils.py

---
+++ math_utils.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("não é possivel dividir por 0.")
+    return a / b
